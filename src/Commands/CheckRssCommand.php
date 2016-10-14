5051b767cc1859fc0ef5d820a7f5b293856993d0
Pavel Belyaev <ksyddd@gmail.com>
2016-10-14 20:51:34 +0400

    Commands

--- src/Commands/CheckRssCommand.php
+++ src/Commands/CheckRssCommand.php
@@ -2,13 +2,32 @@
 
 namespace LaravelNewsVkCommunity\Commands;
 
+use SimplePie;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use LaravelNewsVkCommunity\Database;
 
 class CheckRssCommand extends Command
 {
     /**
+     * @var Database
+     */
+    private $database;
+
+    /**
+     * @var SimplePie
+     */
+    private $feed;
+
+    /**
+     * @var int
+     */
+    private $added = 0;
+
+    /**
+     * Configure.
+     *
      * @return void
      */
     public function configure()
@@ -16,6 +35,10 @@ class CheckRssCommand extends Command
         $this
             ->setName("rss:check")
             ->setDescription("Check LaravelNews's RSS feed.");
+
+        $this->database = new Database;
+        $this->feed = new SimplePie();
+        $this->feed->set_feed_url("http://feed.laravel-news.com/");
     }
 
     /**
@@ -23,9 +46,47 @@ class CheckRssCommand extends Command
      * @param OutputInterface $output
      *
      * @return void
+     * @throws \Exception
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Successfully");
+        $this->feed->init();
+
+        if (!$this->feed->get_items()) {
+            throw new \Exception("Something went wrong...");
+        }
+
+        $toPost = [];
+
+        foreach ($this->feed->get_items() as $item) {
+            $title = $item->get_title();
+            $url = $item->get_links()[0];
+            $tags = [];
+
+            if (count($item->get_categories())) {
+                foreach($item->get_categories() as $category) {
+                    $tags[] = sprintf("#%s", $category->get_term());
+                }
+            }
+
+            $toPost[] = compact('title', 'url', 'tags');
+        }
+
+        if (count($toPost) > 0) {
+            $toPost = array_reverse($toPost);
+
+            foreach($toPost as $post) {
+                if (!$this->database->hasPost($post['url'])) {
+                    $this->database->addPost($post['title'], $post['url'], implode(" ", $post['tags']));
+                    $this->added++;
+                }
+            }
+        }
+
+        if ($this->added > 0) {
+            $output->writeln(sprintf("There was/were %d new item(s)", $this->added));
+        } else {
+            $output->writeln("There is nothing new since the last check");
+        }
     }
 }
---
+++ src/Commands/PostCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace LaravelNewsVkCommunity\Commands;
+
+use GuzzleHttp\Client;
+use LaravelNewsVkCommunity\Database;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PostCommand extends Command
+{
+    /**
+     * @var Database
+     */
+    private $database;
+
+    /**
+     * @var Client
+     */
+    private $guzzle;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    public function configure()
+    {
+        $this
+            ->setName("posts:post")
+            ->setDescription("Post unposted posts.");
+
+        $this->database = new Database;
+        $this->guzzle = new Client();
+        $this->config = require_once __DIR__ . "/../../config.php";
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $posts = $this->database->getPosts();
+
+        if (count($posts)) {
+            foreach ($posts as $post) {
+                $this->publish($post);
+            }
+
+            $output->writeln("Something was published.");
+        } else {
+            $output->writeln("Nothing to publish was found.");
+        }
+    }
+
+    /**
+     * @param array $post
+     */
+    private function publish(array $post)
+    {
+        $request = $this->guzzle->get("https://api.vk.com/method/wall.post", [
+            'query' => [
+                'owner_id' => $this->config['vk']['group_id'],
+                'from_group' => 1,
+                'message' => $post['tags'] . " #LaravelNews #Laravel #PHP",
+                'attachments' => $post['url'],
+                'access_token' => $this->config['vk']['access_token']
+            ]
+        ]);
+
+        $response = json_decode($request->getBody()->getContents(), true);
+
+        if ($request->getStatusCode() == 200 && isset($response['response']['post_id'])) {
+            $this->database->postWasPublished($post['id']);
+        }
+    }
+}
